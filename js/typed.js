9e15f33f5381d81ee235a71c91a000d66eddc575
Czowek <154842302+Czowek@users.noreply.github.com>
2023-12-26 03:21:32 +0100

    Add files via upload

---
+++ js/typed.js
@@ -0,0 +1,11 @@
+// Typing Animation
+const typed = new Typed('.typed-text', {
+    strings: ['Web Developer', 'Python Coder', 'Learner', 'AI Enthusiast'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+  });
+  
+  
